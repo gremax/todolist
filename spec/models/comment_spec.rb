f56307a1e2afbf01a9480cac00a3527211fa615d
Max Grechko <max@grechko.org>
2015-10-28 11:28:33 +0200

    Add TravisCI

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Comment, type: :model do
     it { should belong_to :task }
     it { should have_many(:attachments).dependent(:destroy) }
   end
+
+  describe 'attachments' do
+    before do
+      @comment = create(:comment)
+    end
+
+    it 'should have multiple attachments' do
+      expect { @comment.attachments.create(file: File.open(File.join(Rails.root,
+        '/spec/support/uploads/rg_test_task_grid.png'))) }.
+        to change(@comment.attachments, :count).by(1)
+    end
+  end
 end
